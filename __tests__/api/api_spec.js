ed99ebd3776b1cf4ddc7fd1028fcb9ff6e19301b
Sinan <ulug.1177@gmail.com>
2021-01-28 17:43:32 +0300

    added sorted method

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -3,15 +3,12 @@ const _ = require("lodash");
 const Joi = frisby.Joi;
 var assert = require("assert");
 
-
 it('Verify that status is 200 and content type must be application-json', function (done, response) {
     jest.setTimeout(120000)
     return frisby.get('https://restcountries.eu/rest/v2')
         .expect('status', 200)
         .expect('header', 'Content-Type', 'application/json;charset=UTF-8')
-        //.expect('json', 'RestResponse')
         .done(done);
-
 });
 
 it('Result array lengt should be 250 char.', function (done) {
@@ -26,7 +23,7 @@ it('Result array lengt should be 250 char.', function (done) {
 });
 
 
-it('Alpha code control', function () {
+it('Alphacode control', function () {
     jest.setTimeout(1200000)
     return frisby
         //.get('https://restcountries.eu/rest/v2/name/Turkey')
@@ -35,3 +32,32 @@ it('Alpha code control', function () {
         .expect('json', '?.alpha2Code', 'TR')
         .expect('json', '?.alpha3Code', 'TUR')
 });
+
+it('Array list should be sorted by name', function (done) {
+    jest.setTimeout(1200000)
+    return frisby
+        .get('https://restcountries.eu/rest/v2')
+        .then(function (response) {
+            const responseArrayLength = response.json;
+            console.log(isSorted(responseArrayLength));
+
+        }).done(done)
+});
+
+function isSorted(array) {
+    return array.every((item, i) => {
+        if (i === 0) {
+            return true;
+        }
+        const prevItem = array[i - 1];
+        const result = prevItem.name.localeCompare(
+            item.name
+        ) === -1
+
+        if (!result) {
+            console.log({prevItemName: prevItem.name, itemName: item.name})
+        }
+        return result;
+    });
+}
+
